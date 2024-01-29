89a6c4a24251c6af470b57b26f48aa03bfc90564
“CaesarBourne” <caesaradek@gmail.com>
2024-01-29 15:55:00 +0100

    connecting to hardhat addres s with ether sto increment and decrement

--- js-contracts/ethers_contract.js
+++ js-contracts/ethers_contract.js
@@ -10,31 +10,6 @@ const contract_Address = "0x3409FFbB89260d7EA2d6C9851e123e4d2b41Be85";
 let provider = ethers.getDefaultProvider(process.env.SEP_NETWORK);
 let sender_wallet = new ethers.Wallet(sender_pvt_key, provider);
 
-const contract_ABI = [
-  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
-  {
-    inputs: [],
-    name: "decrement",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "increment",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "variable",
-    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
-    stateMutability: "view",
-    type: "function",
-  },
-];
-
 const contract = new ethers.Contract(contract_Address, contract_ABI, provider);
 console.log("contract ad ", contract_Address);
 console.log("contract abi ", contract_ABI);
